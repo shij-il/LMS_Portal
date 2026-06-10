e90a28381a525b2d7911daf4d778da03e05b824d
MUHAMMED SHIJIL MK <shijil@madeit.ac.in>
2026-06-10 12:26:57 +0530

    fix api url

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "https://lms-portal-bice-rho.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
